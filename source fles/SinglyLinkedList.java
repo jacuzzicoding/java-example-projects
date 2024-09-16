e5cc2ac9551296976981173088037c77970d6ca0
Brock Jenkinson <brockjenkinson@gmail.com>
2024-09-15 21:43:01 -0400

    Working on code

--- source fles/SinglyLinkedList.java
+++ source fles/SinglyLinkedList.java
@@ -23,12 +23,14 @@ public class SinglyLinkedList<E> {
 
     // Other methods (to be implemented)
     // e.g., add, delete, etc.
-
-    // Example method: Add to the beginning of the list
-    public void addFirst(E e) {
-        head = new Node<>(e, head);
-    }
-
+    // this is the method to add an element to the beginning of the list
+    public void addFirst (E,e){
+    head = new Node<>(e,head); 
+    if size == 0  //check if the list is empty
+    tail = head; //if the list is empty then we need to make the tail point to the head 
+    size++; //increment the size of the list to keep track of the number of elements in the list
+}
+  
     // Method to get the first element
     public E first() {
         if (isEmpty()) return null;
