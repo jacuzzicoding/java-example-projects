76b471845d0dda9d09e50d66013edcb11b685bcb
Brock Jenkinson <brockjenkinson@gmail.com>
2024-09-22 21:36:13 -0400

    fixed indentation

--- source fles/GameEntry.java
+++ source fles/GameEntry.java
@@ -25,42 +25,42 @@ public class GameEntry {
     }
 
     /* Implementing the HighScoreStack class here to make it a stack */ 
-    public class HighScoreStack {
-        private GameEntry[] stack;
-        private int top;
-        private static final int CAPACITY = 10;  // just using 10 for simplicity
-    
-        public HighScoreStack() {
-            stack = new GameEntry [CAPACITY];   // Initializing the stack 
-            top = -1;       //we will create an empty stack                                   
-        }
-        public void push(GameEntry entry) {
-            if (top == stack.length - 1) { //exception for if the stack is full
-                System.out.println(" The stack is full. You cannot add more elements. Use pop() to remove an element.");
-                return;
-            }
-            top++; //increment the top pointer
-            stack[top] = entry; // this will add the element to the top of the stack
-        }
-        public GameEntry pop() {
-           if (isEmpty()) {   //exception for if the stack is empty
-               System.out.println("The stack is empty. There are no elements to pop.");
-               return null;  }
-            GameEntry entry = stack[top]; //this will remove the top element
-            stack[top] = null; //this will remove the element from the stack
-            top--; //decrement the top pointer
-            return entry; //return the element that was removed
-        }
-        public GameEntry top() { //this will return but not remove the top element
-            if (isEmpty()) { //exception for if the stack is empty
-                System.out.println("The stack is empty. There are no elements to return.");
-                return null; }
-            return stack[top]; //return the top element
-        }
-        public int size() { //this will return the size of the stack
-            return top + 1;
-            }
-        public boolean isEmpty() { //this will check if the stack is empty
-            return top == -1; //return true if the stack is empty
-        }
+public class HighScoreStack {
+    private GameEntry[] stack;
+    private int top;
+    private static final int CAPACITY = 10;  // just using 10 for simplicity
+
+    public HighScoreStack() {
+        stack = new GameEntry [CAPACITY];   // Initializing the stack 
+        top = -1;       //we will create an empty stack                                   
     }
+    public void push(GameEntry entry) {
+        if (top == stack.length - 1) { //exception for if the stack is full
+            System.out.println(" The stack is full. You cannot add more elements. Use pop() to remove an element.");
+            return;
+        }
+        top++; //increment the top pointer
+        stack[top] = entry; // this will add the element to the top of the stack
+    }
+    public GameEntry pop() {
+        if (isEmpty()) {   //exception for if the stack is empty
+            System.out.println("The stack is empty. There are no elements to pop.");
+            return null;  }
+        GameEntry entry = stack[top]; //this will remove the top element
+        stack[top] = null; //this will remove the element from the stack
+        top--; //decrement the top pointer
+        return entry; //return the element that was removed
+    }
+    public GameEntry top() { //this will return but not remove the top element
+        if (isEmpty()) { //exception for if the stack is empty
+            System.out.println("The stack is empty. There are no elements to return.");
+            return null; }
+        return stack[top]; //return the top element
+    }
+    public int size() { //this will return the size of the stack
+        return top + 1;
+        }
+    public boolean isEmpty() { //this will check if the stack is empty
+        return top == -1; //return true if the stack is empty
+    }
+}
