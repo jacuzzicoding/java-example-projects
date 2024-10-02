049760c9709de7a1212d1e3a63875752a5d9c030
Brock Jenkinson <brockjenkinson@gmail.com>
2024-10-02 14:16:14 -0400

    fixed isEmpty method
    Now using size == 0

--- source fles/queueArray.java
+++ source fles/queueArray.java
@@ -1,3 +1,5 @@
+import java.util.EmptyQueueException;
+
 public class queueArray { //constructor i will use to make the queue
     //need class variables
         //pointers etc
@@ -6,23 +8,27 @@ public class queueArray { //constructor i will use to make the queue
     int rear; //this will point to the tail element
     int size; //this will track how many elements we have
     int capacity; //this will store the maximum size of the array
+
     //structure  x
 
-    public void push(E element) { //need to update this to be enqueue
-        list.addFirst(element); // Add the element to the front of the list (top of the stack)
+    public void enqueue(int element) { //need to update this to be enqueue
+        queue.addFirst(); // Add the element to the front of the queue
     }
 
     //dequeue method will go here
-    
-    public E top() {
+
+    public E first() {
         if (isEmpty()) {
-            throw new EmptyStackException(); // Throw an exception if the stack is empty
+            throw new EmptyQueueException(); // Throw an exception if the stack is empty
         }
-        return list.getFirst(); // Return the first element of the list (top of the stack)
+        return queue.getFirst(); // Return the first element of the list (top of the stack)
     }
 
     public boolean isEmpty() { //this will check if the queue is empty
-        return top == -1; //return true if the stack is empty
+        return size == 0; //return true if the queue is empty
     }
-    //isfull
+
+    //isfull method will go here
+
+    //size method will go here
 }
