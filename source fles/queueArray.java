55940558dedbe872810db0aff1ded672dac334ea
Brock Jenkinson <brockjenkinson@gmail.com>
2024-10-02 13:40:57 -0400

    Adding variable space

---
+++ source fles/queueArray.java
@@ -0,0 +1,13 @@
+//here is where the variables will go
+
+
+
+public class queueArray { //constructor i will use to make the queue
+    //need class variables
+        //pointers etc
+    //structure  x
+    //enqueue method
+    //dequeue method
+    //isempty
+    //isfull
+}
